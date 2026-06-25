2e5157e7fda56a72f93113967ded5a67e3c59fd9
Luciano Miranda <lu.mer.zoni@gmail.com>
2026-06-25 20:52:50 -0300

    cambios en las bases

--- FRONTEND/Configuracion/config.js
+++ FRONTEND/Configuracion/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://eleve-barberia-xi.vercel.app/api/v1';
+export const API_BASE_URL = 'https://demo-tesis.vercel.app/api/v1';
 /* export const API_BASE_URL = 'http://localhost:3000/api/v1'; */
